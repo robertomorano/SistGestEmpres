cb0943a81a232e3c655de9be66f91f0ef8016ec6
roberto.morano <roberto.morano@107-27>
2025-10-08 14:17:06 +0200

    crear

---
+++ HolaMundo/HolaMundo/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HolaMundo._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <asp:Label ID="labelTexto" runat="server" Text="ola mlundo"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" Text="Hola"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button2_Click" />
+        <asp:Label ID="labelError" runat="server" Text="" ForeColor="IndianRed"></asp:Label>
+
+    </main>
+
+</asp:Content>
---
+++ HolaMundo/HolaMundo/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HolaMundo
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TextBox1.Text)){
+
+                labelTexto.Text = "Hola, " + TextBox1.Text;
+                TextBox1.Text = "";
+                labelError.Text = "";
+            }
+            else
+            {
+                labelError.Text = "Nombre no ponido";
+            }
+        }
+
+        
+    }
+}
