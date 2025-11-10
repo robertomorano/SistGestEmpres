f2dac41f7bd46e37660f5b7408dd35d9e3974bf1
robmorx <182227050+robertomorano@users.noreply.github.com>
2025-11-10 22:18:57 +0100

    temudate

---
+++ Tema8/Mandalorian/CompositionRoot/DI.cs
@@ -0,0 +1,10 @@
+﻿namespace CompositionRoot
+{
+    public class DI
+    {
+
+    }
+}
+
+
+
--- Tema8/Mandalorian/UI/obj/Debug/net8.0/UI.AssemblyInfo.cs
+++ Tema8/Mandalorian/UI/obj/Debug/net8.0/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc4210bb07a177ff34a4b395abf54364292c2c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934247d9d3b590d230002eca9cc0d74fbbddd791")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
